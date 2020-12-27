e77f4ee020db76bf786a6b150ff2041d93eb70d9
Audrey M <kangaroo.queen88@gmail.com>
2020-12-26 23:40:12 -0500

    Fixed race condition to work properly with testing suite

--- directoryscan_test.go
+++ directoryscan_test.go
@@ -24,7 +24,7 @@ var _ = Describe("CreditCardScan", func() {
 	Describe("Scanning a folder with 2 known Credit Card findings", func() {
 		Context("less than 100 MB in size", func() {
 			It("should return 2 findings", func() {
-				results, _ := directoryscanner.Find("./credit_card", "Credit Card")
+				results, _ := directoryscanner.Find("./secret", "Credit Card")
 				Expect(len(results)).To(Equal(2))
 			})
 		})
@@ -32,14 +32,49 @@ var _ = Describe("CreditCardScan", func() {
 })
 
 // Test for SSN
-// var _ = Describe("SSNScan", func() {
-// 	Describe("Scanning a folder with 4 known SSN findings", func() {
-// 		Context("less than 100 MB in size", func() {
-// 			It("should return 4 findings", func() {
-// 				results1, _ := directoryscanner.Find("./secret", "SSN")
-// 				//fmt.Println("results", len(results))
-// 				Expect(len(results1)).To(Equal(2))
-// 			})
-// 		})
-// 	})
-// })
+var _ = Describe("SSNScan", func() {
+	Describe("Scanning a folder with 4 known SSN findings", func() {
+		Context("less than 100 MB in size", func() {
+			It("should return 4 findings", func() {
+				results, _ := directoryscanner.Find("./secret", "SSN")
+				Expect(len(results)).To(Equal(4))
+			})
+		})
+	})
+})
+
+// Test for Word Password
+var _ = Describe("PasswordScan", func() {
+	Describe("Scanning a folder with 1 known \"Word Password\" finding", func() {
+		Context("less than 100 MB in size", func() {
+			It("should return 1 finding", func() {
+				results, _ := directoryscanner.Find("./secret", "Word Password")
+				Expect(len(results)).To(Equal(1))
+			})
+		})
+	})
+})
+
+// Test for Word Username
+var _ = Describe("UsernameScan", func() {
+	Describe("Scanning a folder with 1 known \"Word Username\" finding", func() {
+		Context("less than 100 MB in size", func() {
+			It("should return 1 finding", func() {
+				results, _ := directoryscanner.Find("./secret", "Word Username")
+				Expect(len(results)).To(Equal(1))
+			})
+		})
+	})
+})
+
+// Test for Multiscan
+var _ = Describe("MultipleScan", func() {
+	Describe("Scanning a folder with 7 known total findings of SSN, Word Username, and Credit Card types", func() {
+		Context("less than 100 MB in size", func() {
+			It("should return 7 findings", func() {
+				results, _ := directoryscanner.Find("./secret", "SSN", "Word Username", "Credit Card")
+				Expect(len(results)).To(Equal(7))
+			})
+		})
+	})
+})
--- directoryscanner.go
+++ directoryscanner.go
@@ -8,6 +8,7 @@ import (
 	"path/filepath"
 	"regexp"
 	"strconv"
+	"sync"
 )
 
 var compiledRegexes = map[string][]*regexp.Regexp{
@@ -20,8 +21,8 @@ var compiledRegexes = map[string][]*regexp.Regexp{
 
 // will hold results from the scan
 var resultsScan []string
-var resultsFind []string
 var selectedTypes []string
+var mu sync.Mutex
 
 func scanFiles(path string, info os.FileInfo, err error) error {
 
@@ -62,7 +63,7 @@ func findSpecific(path string, info os.FileInfo, err error) error {
 						for _, r := range cr {
 							if found := r.Find([]byte(fscanner.Text())); found != nil {
 								resultsString = key + `,` + string(found) + `,` + file.Name() + `,` + strconv.Itoa(lineNumber)
-								resultsFind = append(resultsFind, resultsString)
+								resultsScan = append(resultsScan, resultsString)
 							}
 						}
 					} else {
@@ -80,7 +81,9 @@ func findSpecific(path string, info os.FileInfo, err error) error {
 // Scan ...
 // begins at the specified path (path) and recursively searches all directories
 // acceptable arguments for variatic function: "Credit Card", "SSN", "Word Password", "Word Username", "Email"
-func Scan(path string, dataTypes ...string) ([]string, error) {
+func Scan(path string) ([]string, error) {
+	mu.Lock()
+	defer mu.Unlock()
 	resultsScan = nil
 	err := filepath.Walk(path, scanFiles)
 	if err != nil {
@@ -91,12 +94,14 @@ func Scan(path string, dataTypes ...string) ([]string, error) {
 
 // Find ...
 func Find(path string, dataTypes ...string) ([]string, error) {
-	resultsFind = nil
-	resultsFind = make([]string, 0)
+	mu.Lock()
+	defer mu.Unlock()
+	resultsScan = nil
+	selectedTypes = nil
 	selectedTypes = append(selectedTypes, dataTypes...)
 	err := filepath.Walk(path, findSpecific)
 	if err != nil {
 		return nil, err
 	}
-	return resultsFind, nil
+	return resultsScan, nil
 }
