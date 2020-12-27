b157394308ce2b2ede6bfef49da28fd82cc172ff
Audrey M <kangaroo.queen88@gmail.com>
2020-12-27 09:11:23 -0500

    Updated README, made the regexes modifiable

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # directoryscanner
+**A simple go module for scanning directories for sensitive information (or really anything you want)
+
+##How to Use
+'''
+import github.com/audrey-morrisette/directoryscanner
+...
+directoryscanner.Scan(".")
+'''
+
+`Scan()` returns a slice of strings containing all findings from the given directory
+
+##TODO:
+- [ ] Add simpler way to add new items to scan for
+- [ ] Performance Improvements
--- directoryscanner.go
+++ directoryscanner.go
@@ -11,7 +11,8 @@ import (
 	"sync"
 )
 
-var compiledRegexes = map[string][]*regexp.Regexp{
+// CompiledRegexes ...
+var CompiledRegexes = map[string][]*regexp.Regexp{
 	"Credit Card":   {regexp.MustCompile("^(?:4[0-9]{12}(?:[0-9]{3})?|[25][1-7][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\\d{3})\\d{11})$")},
 	"SSN":           {regexp.MustCompile("(^\\d{3}-?\\d{2}-?\\d{4}$|^XXX-XX-XXXX$)")},
 	"Word Password": {regexp.MustCompile("password")},
@@ -33,7 +34,7 @@ func scanFiles(path string, info os.FileInfo, err error) error {
 	// skip the source code
 	if file.Name() != "directory_scanner.go" {
 		for fscanner.Scan() {
-			for key, cr := range compiledRegexes {
+			for key, cr := range CompiledRegexes {
 				for _, r := range cr {
 					if found := r.Find([]byte(fscanner.Text())); found != nil {
 						resultsString = key + `,` + string(found) + `,` + file.Name() + `,` + strconv.Itoa(lineNumber)
@@ -58,7 +59,7 @@ func findSpecific(path string, info os.FileInfo, err error) error {
 	if file.Name() != "directory_scanner.go" {
 		for fscanner.Scan() {
 			for _, dataType := range selectedTypes {
-				for key, cr := range compiledRegexes {
+				for key, cr := range CompiledRegexes {
 					if dataType == key {
 						for _, r := range cr {
 							if found := r.Find([]byte(fscanner.Text())); found != nil {
