65b95de020c41ce4781ce289b40adf16515f9e4b
Audrey M <kangaroo.queen88@gmail.com>
2020-12-29 09:23:02 -0500

    Removed external access to compiled rgeexes; redundant functionality with FindString implemented

--- directoryscanner.go
+++ directoryscanner.go
@@ -1,5 +1,5 @@
 // Package directoryscanner provides simple directory scanning functionality
-// for sensitive data
+// for sensitive data, or for any other string/regex you want
 package directoryscanner
 
 import (
@@ -11,9 +11,8 @@ import (
 	"sync"
 )
 
-// CompiledRegexes ...
-// Accessible outside the module so users can add (or remove) whatever they want
-var CompiledRegexes = map[string][]*regexp.Regexp{
+// doesn't need to be externally visible; functionality in place to use custom slices of strings/regexes
+var compiledRegexes = map[string][]*regexp.Regexp{
 	"Credit Card":   {regexp.MustCompile("^(?:4[0-9]{12}(?:[0-9]{3})?|[25][1-7][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\\d{3})\\d{11})$")},
 	"SSN":           {regexp.MustCompile("(^\\d{3}-?\\d{2}-?\\d{4}$|^XXX-XX-XXXX$)")},
 	"Word Password": {regexp.MustCompile("password")},
@@ -36,7 +35,7 @@ func scanFiles(path string, info os.FileInfo, err error) error {
 	var resultsString string
 
 	for fscanner.Scan() {
-		for key, cr := range CompiledRegexes {
+		for key, cr := range compiledRegexes {
 			for _, r := range cr {
 				if found := r.Find([]byte(fscanner.Text())); found != nil {
 					resultsString = key + `,` + string(found) + `,` + file.Name() + `,` + strconv.Itoa(lineNumber)
@@ -60,7 +59,7 @@ func findSpecific(path string, info os.FileInfo, err error) error {
 
 	for fscanner.Scan() {
 		for _, dataType := range selectedTypes {
-			for key, cr := range CompiledRegexes {
+			for key, cr := range compiledRegexes {
 				if dataType == key {
 					for _, r := range cr {
 						if found := r.Find([]byte(fscanner.Text())); found != nil {
